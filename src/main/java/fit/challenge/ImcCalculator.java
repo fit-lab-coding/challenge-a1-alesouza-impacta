7201f126631f85cbfb60ee42dca8aa9d3b8dad20
alesouza-impacta <alessandra.souza@aluno.faculdadeimpacta.com.br>
2021-10-30 07:44:07 -0300

    criação da classe

---
+++ src/main/java/fit/challenge/ImcCalculator.java
@@ -0,0 +1,13 @@
+package fit.challenge;
+
+public class ImcCalculator {
+
+    static String calculate(int valor1, double valor2){
+        return null;
+    }
+
+    static String calculate(double valor1, double valor2){
+        return null;
+    }
+    
+}
