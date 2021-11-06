1fbafe4421b82cda86cbe365c327929fb8db8449
alesouza-impacta <alessandra.souza@aluno.faculdadeimpacta.com.br>
2021-11-05 21:11:37 -0300

    ajuste de testes

--- src/main/java/fit/challenge/ImcCalculator.java
+++ src/main/java/fit/challenge/ImcCalculator.java
@@ -2,12 +2,23 @@ package fit.challenge;
 
 public class ImcCalculator {
 
-    static String calculate(int valor1, double valor2){
-        return null;
+    static String calculate(int peso, double altura){
+       double resultado = peso/(altura*altura);
+       if (resultado > 25.0 || resultado <= 29.9)
+        return "Sobrepeso";
+       return null;
     }
 
-    static String calculate(double valor1, double valor2){
-        return null;
+    static String calculate(double peso, double altura){
+        double resultado = peso/(altura*altura);
+        if (resultado < 18.5)
+            return "Magreza";
+        else if (resultado < 24.9) 
+            return "Normal";
+        else if (resultado < 30.0) 
+            return "Sobrepeso";
+        else 
+            return "Obesidade";            
     }
     
 }
